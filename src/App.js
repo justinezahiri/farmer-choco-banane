de922b67b4fd16d16a2eabe41986f33aef9b56b2
justinezahiri <justinezahiri@gmail.com>
2019-06-02 11:08:49 +0200

    implemented page basic structure

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import "./custom.scss";
 function App() {
   return (
     <div className="App">
-      <h1 className="title">
-        Pour le retour des farmers chocolat noir banane!
-      </h1>
+      <section class="product">
+        <h1>Pour le retour des farmers chocolat noir banane!</h1>
+      </section>
+
+      <section className="story">
+        <h1>pourquoi c'était bien?</h1>
+      </section>
+      <section className="action">
+        <h1>comment nous aider?</h1>
+      </section>
     </div>
   );
 }
